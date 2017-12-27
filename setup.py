2898e1c8f3c2a5ff3d5044622587e6ed8073fe08
administrador <administrador@yordy-gearplug.168.10.169>
2017-12-27 15:28:39 -0500

    archivo setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='infusionsoft-python',
+      version='0.1',
+      description='API wrapper for Infusionsoft written in Python',
+      url='https://github.com/GearPlug/infusionsoft-python',
+      author='Yordy Gelvez',
+      author_email='yordy.gelvez@gmail.com',
+      license='GPL',
+      packages=['infusionsoft-python', ],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
